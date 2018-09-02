78e7e977b7cdb91b3588a3bac2f063e81a78145f
Michal Frystacky <Michal.Frystacky@gmail.com>
2018-09-02 14:59:56 -0800

    Feat: Lightbox that renders any image in the gallery

--- src/Lightbox.js
+++ src/Lightbox.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Modal from '@material-ui/core/Modal';
+import _ from 'lodash';
 import type {image} from './api/getImages';
 import Image from './Image';
 
@@ -11,13 +12,20 @@ type Props = {
   handleClose: Function,
 };
 
-const Lightbox = ({imageData, open, handleClose}: Props) => (
-  <Modal
-    aria-labelledby={`Full page view of image ${imageData.altText}`}
-    aria-describedby="Full page modal view of specific image with transition capability"
-    open={open}
-    onClose={handleClose}>
-    <Image {...imageData} />
-  </Modal>
-);
+const Lightbox = ({imageData, open, handleClose}: Props) => {
+  if (_.isEmpty(imageData, true)) {
+    return <div />;
+  }
+  return (
+    <Modal
+      aria-labelledby={`Full page view of image ${imageData.altText}`}
+      aria-describedby="Full page modal view of specific image with transition capability"
+      open={open}
+      onClose={handleClose}>
+      <div>
+        <Image {...imageData} />
+      </div>
+    </Modal>
+  );
+};
 export default Lightbox;
--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -6,7 +6,9 @@ import withWidth from '@material-ui/core/withWidth';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Lightbox from './Lightbox';
 import getImages from './api/getImages';
+import type {image} from './api/getImages';
 
 const styles = theme => ({
   root: {
@@ -50,6 +52,10 @@ type State = {
   },
   loading: boolean,
   modalOpen: boolean,
+  modalData: {
+    tile: image,
+    position: number,
+  },
 };
 
 class PhotoGallery extends Component<Props, State> {
@@ -66,14 +72,20 @@ class PhotoGallery extends Component<Props, State> {
       },
       loading: true,
       modalOpen: false,
+      modalData: {imageData: {}, position: -1},
     };
   }
 
   componentDidMount() {
     setTimeout(() => {
       const images = getImages();
-      const renderedTiles = images.map(tile => (
-        <GridListTile key={tile.key} cols={tile.cols || 1}>
+      const renderedTiles = images.map((tile, i) => (
+        <GridListTile
+          key={tile.key}
+          cols={tile.cols || 1}
+          onClick={e => {
+            this.handleOpenModal(e, tile, i);
+          }}>
           <img src={tile.url} alt={tile.altText || tile.caption} />
         </GridListTile>
       ));
@@ -81,16 +93,39 @@ class PhotoGallery extends Component<Props, State> {
     }, 1500);
   }
 
+  handleOpenModal = (e, tile, i) => {
+    console.log(e.target, tile, i);
+    this.setState({
+      modalData: {imageData: tile, position: i},
+      modalOpen: true,
+    });
+  };
+
   handleCloseModal = () => {
     this.setState({modalOpen: false});
   };
 
   render() {
     const {classes, width} = this.props;
-    const {columnsRender, loading, renderedTiles} = this.state;
-
+    const {
+      columnsRender,
+      loading,
+      renderedTiles,
+      modalOpen,
+      modalData,
+    } = this.state;
+    console.log(this.state);
     return (
       <div className={classes.root}>
+        {modalOpen ? (
+          <Lightbox
+            imageData={modalData.imageData}
+            open={modalOpen}
+            handleClose={this.handleCloseModal}
+          />
+        ) : (
+          ''
+        )}
         {loading ? (
           <CircularProgress className={classes.progress} />
         ) : (
