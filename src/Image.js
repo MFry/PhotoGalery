b6c90548f0440c14e624134aa59822dfb2683be5
Michal Frystacky <Michal.Frystacky@gmail.com>
2018-09-03 22:23:36 -0800

    Chore: Added caption test data

--- src/Image.js
+++ src/Image.js
@@ -8,7 +8,11 @@ type Props = {
 };
 
 const Image = ({url, altText, caption}: Props) => (
-  <img src={url} alt={altText || caption} />
+  <img
+    src={url}
+    alt={altText || caption}
+    style={{maxHeight: '100vh', maxWidth: '100vw'}}
+  />
 );
 
 export default Image;
